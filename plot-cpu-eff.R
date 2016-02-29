59f2c8f4b96c3b900520a09767077f8e8fd093bd
Mishura <rotke@ya.ru>
2016-03-01 01:34:27 +0300

    final

--- plot-cpu-eff.R
+++ plot-cpu-eff.R
@@ -33,32 +33,21 @@ getwd()
 
 #sessionInfo()
 library(ggplot2)
-cpu_info <- read.xlsx("/Users/mrotkevich/Desktop/DATA/cpu-stats.xls", header = T,sheetName = "page1")
+#cpu_info <- read.xlsx("/Users/mrotkevich/Desktop/DATA/cpu-stats.xls", header = T,sheetName = "page1")
+cpu_info <- read.xlsx("/Users/mrotkevich/Desktop/DATA/Excel_Workbook.xls", header = F,1)
 
 cpu_num <- c()
-cpu_info1<-na.omit(cpu_info[cpu_info$CPU != 'Min:' &cpu_info$Step != 'Total:' ,])
-cpu_info1<-cpu_info1[c("CPU","Step","Avg")]
+#cpu_info1<-na.omit(cpu_info[cpu_info$CPU != 'Min:' &cpu_info$Step != 'Total:' ,])
 
-#plot(cpu_info1$Step,cpu_info1$Avg,"l",col='black',xlab = 'Step', ylab = 'Seconds',xlim=c(0, 550), ylim=c(0, 100))
-mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
-                      labels=c("3gears","4gears","5gears")) 
-mtcars$am <- factor(mtcars$am,levels=c(0,1),
-                    labels=c("Automatic","Manual")) 
-mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
-                     labels=c("4cyl","6cyl","8cyl")) 
-
-
-
-
-long_cpu<-cpu_info1 %>% gather(Observation, Time, -CPU,-Step)
-long_cpu<-long_cpu[long_cpu$Observation != 'Avg',]
-long_cpu$CPU<-factor( as.numeric(as.character(long_cpu$CPU)),labels=cpu_num)
-long_cpu$Step<-factor( as.numeric(as.character(long_cpu$Step)),labels = c(50,100,150,200,250,300,350,400,450,500))
-
+long_cpu<-cpu_info %>% gather(Observation, Time, -X2,-X1)
+#long_cpu<-long_cpu[long_cpu$Observation != 'Avg',]
+long_cpu$X1<-factor( as.numeric(as.character(long_cpu$X1)),labels=unique(long_cpu$X1))
+long_cpu$X2<-factor( as.numeric(as.character(long_cpu$X2)),labels = unique(long_cpu$X2))
+names(long_cpu)[1]<-'CPU'
+names(long_cpu)[2]<-'Step'
 cpus <- summarySE(long_cpu,
                    measurevar="Time",
                    groupvars=c("CPU","Step"))
-qplot(Step, Time, data=long_cpu, geom=c("point", "smooth"), method="lm")
 
 cpu_plot <- ggplot(cpus, aes(x=Step, y=Time, colour=CPU)) +
   geom_errorbar(aes(ymin=Time-se, ymax=Time+se, group=CPU),
@@ -68,13 +57,3 @@ cpu_plot <- ggplot(cpus, aes(x=Step, y=Time, colour=CPU)) +
 cpu_plot
 
 
-for (j in 1:9) {
-  #cpu_info_2<-na.omit(cpu_info[cpu_info$CPU == j &cpu_info$Step != 'Total:' ,])
-  #cpu_info_2$Step<-as.numeric(as.character(cpu_info_2$Step))
-  #lines(cpu_info_2$Step,cpu_info_2$Avg,col = j)
-  cpu_num<-c(cpu_num,paste('cpu',j))
-}
-legend(x="topright", legend = cpu_num, col = 2:9,
-       lty=rep(1,7))
-title("Effeciency",
-      cex.main = 2,   font.main= 1, col.main= "black")
