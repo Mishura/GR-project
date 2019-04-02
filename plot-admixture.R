6cff7fe5ab4e65fc98327f2a0e580ccf8a71586e
Mishura <rotke@ya.ru>
2019-04-02 18:42:53 +0300

    added comments

--- plot-admixture.R
+++ plot-admixture.R
@@ -25,24 +25,16 @@ more_info <- merge(x = sample_data, y = sample_info, by.x = "new_iid", by.y= "V2
 
 family_labels <- sample_info[, 2, drop = F]
 sample_labels <- sample_info$V2
-#continent_gr <- as.vector(sapply(sample_labels, function(x) strsplit(x, "_")[[1]][2]))
-#ethn_label <- as.data.frame(ethn,stringsAsFactors=FALSE,drop=FALSE)
 labels_adm <- 1:length(files);
 k<-0;
-sapply(adm_results, function(x) {rownames(x)<-sample_labels; k<<-k+1; labels_adm[[k]]<<-paste0("K=",k)})
-#rownames(ethn_label) <- sample_labels
+sapply(adm_results, function(x) {rownames(x)<-sample_labels; k<<-k+1; labels_adm[[k]]<<-paste0('K=',sprintf("%02d",k))})
+names(adm_results)<-labels_adm;
 continent_gr <- more_info[, c('continent_region'), drop =F]
 colnames(continent_gr)[colnames(continent_gr)=="continent_region"] <- "continent"
 rownames(continent_gr) <- sample_labels
-#p2 <- plotQ(admx_results2,returnplot=T,exportplot=F,quiet=T,basesize=5,
-#      grplab=continent_gr,grplabsize=1.5,linesize=0.5,pointsize=1,
-#      ordergrp = T,panelratio=c(10,2),splab=c("K=2"))
-
 plotQ(adm_results,imgoutput="join",
       returnplot=T,exportplot=T,quiet=T,basesize=5,
       grplab=continent_gr,grplabjust= 0, grplabsize=1,linesize=0.5,
       pointsize=1, grplabangle = 90, grplabheight= 6,
       showindlab=F,indlabwithgrplab=F, indlabangle =90, indlabsize = 4, sharedindlab=F, 
       ordergrp = T,panelratio=c(10,10),splab=labels_adm,clustercol=kelly)
-#g<-grid.arrange(p2$plot[[1]],p3$plot[[1]],nrow=2)
-#ggsave(file="adm23.pdf", g)
